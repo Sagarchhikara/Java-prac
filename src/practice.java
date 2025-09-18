1d507b0bad524c8224ac887700e5fcd09ff95ec8
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-18 09:39:18 +0530

    Changes made

---
+++ src/practice.java
@@ -0,0 +1,13 @@
+public class practice {
+    // Wap to find the largest element in a given array
+    // wap to input and print a 2d array
+    // wap to copy one array to another
+    // wap to display weather the given array is equal or not without using predefined method
+    // wap to add two matrices
+    // wap to find the transpose of given matrix
+    // wap to find the sum of primary and secondary diagonal of a matrix
+    public static void main(String[] args) {
+
+    }
+
+}
