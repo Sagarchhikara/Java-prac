9f2bcbe44b8b95c904cfeee0879e74d444ff28ab
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-09-24 11:41:12 +0530

    Update linersearch.java

--- src/linersearch.java
+++ src/linersearch.java
@@ -1,10 +1,12 @@
 public class linersearch {
     public static void main (String[] args) {
-        int[] nums={23,11,1,3,6,8,-11,28,67};
-        int target=19;
-        int ans=linearsearch2(nums,target);
-        System.out.println(ans);
-
+//        int[] nums={23,11,1,3,6,8,-11,28,67};
+//        int target=19;
+//        int ans=linearsearch2(nums,target);
+//        System.out.println(ans);
+          String name= "Sagar";
+          char target='r';
+        System.out.println(search(name,target));
 
     }
     // search in the array: return the index if found
@@ -21,18 +23,24 @@ public class linersearch {
 //        // this line will exectue if target not found
 //        return -1;
 //    }
-    static int linearsearch2(int[] arr,int target){
-        if(arr.length==0){
-            return -1;
-        }
-        for(int element:arr){
-            if(element==target){
-                return element;
-
-            }
-        }
-        // this line will exectue if target not found
-        return -1;
+//    static int linearsearch2(int[] arr,int target){
+//        if(arr.length==0){
+//            return -1;
+//        }
+//        for(int element:arr){
+//            if(element==target){
+//                return element;
+//
+//            }
+//        }
+//        // this line will exectue if target not found
+//        return -1;
+//    }
+    static boolean search(String str,char target){
+    if(str.length()==0){
+        return false;
+    }
+    for (int i=0;i<)
     }
 
 }
