8c7b93c86678248194124b2e97ff30bde1867c8c
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-09-29 15:28:18 +0530

    Update selectionsort.java

--- src/selectionsort.java
+++ src/selectionsort.java
@@ -1,31 +1,47 @@
 class selectionsort{
     public static void main(String[] args) {
         int[] arr = {64, 25, 12, 22, 11};
-        selectionSort(arr);
-        System.out.println("Sorted array:");
-        printArray(arr);
+
     }
 
-    static void selectionSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minIdx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIdx]) {
-                    minIdx = j;
-                }
+//    static void selectionSort(int[] arr) {
+//        int n = arr.length;
+//        for (int i = 0; i < n - 1; i++) {
+//            int minIdx = i;
+//            for (int j = i + 1; j < n; j++) {
+//                if (arr[j] < arr[minIdx]) {
+//                    minIdx = j;
+//                }
+//            }
+//            int temp = arr[minIdx];
+//            arr[minIdx] = arr[i];
+//            arr[i] = temp;
+//        }
+//    }
+//
+//    static void printArray(int[] arr) {
+//        for (int num : arr) {
+//            System.out.print(num + " ");
+//        }
+//        System.out.println();
+//
+//    }
+  static  void selection(int[] arr){
+      for (int i = 0; i < arr.length; i++) {
+        int last=arr.length-i-1;
+        int maxindex=getMaxIndex(arr,0,last)
+      }
+  }
+
+    private static int getMaxIndex(int[] arr, int start, int last) {
+        int max=start;
+        for (int i = start; i < last; i++) {
+            if(arr[max]<arr[i]){
+                max=i;
             }
-            int temp = arr[minIdx];
-            arr[minIdx] = arr[i];
-            arr[i] = temp;
-        }
-    }
 
-    static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
         }
-        System.out.println();
-
+    return max;
     }
+    void swap(int[] arr)
 }
