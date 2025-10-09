84cdec0fded82f3ee0530120195e3171f542ee62
Sagarchhikara <sagarchhikara13570@gmail.com>
2025-10-09 11:29:13 +0530

    Update dsoops.java

--- src/dsoops.java
+++ src/dsoops.java
@@ -1,6 +1,16 @@
 public class dsoops {
     public static void main(String[] args){
-        
+        int[] arr=[2,-6,8,10,-1,-5,4,-11,8,9,-10];
+        shiftarray(arr);
 
     }
+
+    private static void shiftarray(int[] arr) {
+
+    }
+    class car{
+
+    }
+
+
 }
