126ee4dbd06792304e5bae9bcc4229270fe87f8d
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-10-23 21:59:38 +0530

    Create cyclicsort.java

---
+++ src/Sorting/cyclicsort.java
@@ -0,0 +1,18 @@
+package Sorting;
+
+public class cyclicsort {
+    public static void main(String[] args) {
+
+    }
+    static void  sort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }else{
+                i++;
+            }
+        }
+    }
+}
