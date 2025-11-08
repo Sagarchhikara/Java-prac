6b592ed832347c8e8243345d6e7ac2f724db86e4
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-11-08 11:33:25 +0530

    Update stack2.java

--- src/stackqueue/stack2.java
+++ src/stackqueue/stack2.java
@@ -1,17 +1,28 @@
 package stackqueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class stack2 {
     public  static  void main(String[] args){
-        Stack<Integer> stack=new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+//        Stack<Integer> stack=new Stack<>();
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);
+//        stack.push(4);
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+       // Queue in java
+        Queue<Integer> queue= new LinkedList<>();
+        queue.add(3);
+        queue.add(23);
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
     }
 }
