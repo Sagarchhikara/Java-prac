1b2223241013e76e7892e1b71aceb72ed9447cf4
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-11-11 10:30:29 +0530

    Update stack1.java

--- src/stackqueue/stack1.java
+++ src/stackqueue/stack1.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 // Other imports go here
 // Do NOT change the class name
 class stack1{
+    static int SIZE=10;
+    static  int[] front=-1;
+    static int rear=-1;
+    static int array[]=new int[SIZE];
+    // method to add item to the queue
+    void enqueue(int item){
+        if(rear==SIZE-1){
+            System.out.println("Queue is full");
+            return;
+        }
+        if(front ==-1){
+            front=rear=0;
+        }
+        else
+            rear++;
+        array[rear]=item;
+        System.out.println(item+"enqueued to queque");
+    }
 
-    protected int[] data;
-    private static  final int DEFAULT_CAPACITY=10;
-    public stack1(){
-        data=new int[DEFAULT_CAPACITY];
-    }
-v saas  d
-    }
+
+}
