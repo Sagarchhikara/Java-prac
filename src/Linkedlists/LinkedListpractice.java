3217f06e7a68364df5bf99173e8d7941e61f7ba4
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-11-23 12:03:33 +0530

    Create LinkedListpractice.java

---
+++ src/Linkedlists/LinkedListpractice.java
@@ -0,0 +1,14 @@
+package Linkedlists;
+
+public class LinkedListpractice {
+    class Node{
+         int data;
+         Node next,prev;
+
+         Node(int data){
+             this.data=
+         }
+    }
+
+
+}
