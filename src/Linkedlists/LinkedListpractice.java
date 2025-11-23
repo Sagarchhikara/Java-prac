a82b414fe37289667db83924802c653b23f1638d
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-11-23 12:12:35 +0530

    Update LinkedListpractice.java

--- src/Linkedlists/LinkedListpractice.java
+++ src/Linkedlists/LinkedListpractice.java
@@ -6,9 +6,32 @@ public class LinkedListpractice {
          Node next,prev;
 
          Node(int data){
-             this.data=
+             this.data=data;
+             this.next=null;
+             this.prev=null;
          }
+    }
+     public void insertFirst(int data){
+        Node newNode= new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head.prev=newNode;
+        head=newNode;
+    }
+    public void insertEnd(int data){
+        Node newNode= new Node(int data);
+        if(head==null){
+            head=newNode;
+        }
+    Node temp=head;
+    while()
     }
 
 
+
+
 }
+ 
