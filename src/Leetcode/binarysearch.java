bc5ae8b7d3b56be34a7bc854e3bdadad0281d7b2
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-12-28 18:26:15 +0530

    ..

--- src/Leetcode/binarysearch.java
+++ src/Leetcode/binarysearch.java
@@ -58,6 +58,20 @@ public class binarysearch {
         return ans;
     }
 // Q 852 https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+        public int peakIndexInMountainArray(int[] arr) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                // You are in decreasing part of array this may be the answwer bu tlook at left
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
 
+        }
+        return start;
+    }
     }
 
