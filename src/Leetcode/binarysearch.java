6c88a89419f9f2d8f942b2188f2ad9ee3b991964
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-12-29 14:35:27 +0530

    ..

--- src/Leetcode/binarysearch.java
+++ src/Leetcode/binarysearch.java
@@ -134,5 +134,57 @@ public class binarysearch {
     }
     // Q 33 https://leetcode.com/problems/find-in-mountain-array/
 
+        static  int binarySearchpivot(int[] nums, int target) {
+            int pivot=findpivot(nums);
+            // if you did not find a pivot means the araay is not roate
+            if(pivot==-1){
+                // normal BS
+                return binarysearch(nums,target,0,nums.length-1);
+
+            }
+            if(nums[pivot]==target){
+                return pivot;
+            }
+
+
+        }
+        static int binarysearch(int[] arr ,int target,int start,int end){
+//        int start =0;
+//        int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(target<arr[mid]){
+                end=mid-1;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+        static int findpivot(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            // 4 cases
+            int mid=start+(end-start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            if(mid>start && arr[mid]<arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid]<=arr[start]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return -1;
+        }
     }
 
