7ba7db7082bf6edef14f973c205d0309c40c5e22
Sagar Chhikara <sagarchhikara13570@gmail.com>
2025-12-31 11:26:30 +0530

    ..

--- src/Sorting/mergesort.java
+++ src/Sorting/mergesort.java
@@ -1,15 +1,14 @@
 package Sorting;
-
+import java.util.*;
 public class mergesort {
     public static void main(String[] args){
         int[] arr={12,11,13,5,6,9,15,86,31};
         System.out.println("Original Array");
         printarr(arr);
-        mergesor(arr);
+        mergeSort(arr);
         System.out.println("Sorted Array");
         printarr(arr);
     }
-
     private static void mergesor(int[] arr) {
         mergeSortUtil(arr,0,arr.length-1);
     }
@@ -60,5 +59,53 @@ public class mergesort {
             arr[k++] = R[j++];
         }
 
+    }
+    // mergesort using recursion
+    static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+
+        return mix;
     }
 }
