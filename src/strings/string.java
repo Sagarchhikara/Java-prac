3fcb4c6a7802b075e23ac8361399a8188e54ecc5
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-01 13:49:17 +0530

    ..

---
+++ src/strings/string.java
@@ -0,0 +1,17 @@
+package strings;
+import  java.util.*;
+public class string {
+    public static void main(String[] args){
+        StringBuilder sb= new StringBuilder("Tony");
+        System.out.println(sb);
+        // Character at index 0
+        System.out.println(sb.charAt(0));
+        // changing character at index 0
+        sb.setCharAt(0,'p');
+        System.out.println(sb);
+        // inserting
+        sb.insert(0,"s");
+        System.out.println(sb);
+        sb.insert(2,'n');
+    }
+}
