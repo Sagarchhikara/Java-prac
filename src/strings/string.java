da4ca0a1c4d69872b6262e12f01ab0a04a75c327
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-01 14:00:32 +0530

    ..

--- src/strings/string.java
+++ src/strings/string.java
@@ -19,6 +19,13 @@ public class string {
         for(int i=0;i<sb.length()/2;i++){
             int front=i;
             int back=sb.length()-1-i;
+            char frontChar=sb.charAt(front);
+            char backchar=sb.charAt(back);
+            sb.setCharAt(front,backchar);
+            sb.setCharAt(back,frontChar);
+
+
         }
+        System.out.println(sb);
     }
 }
