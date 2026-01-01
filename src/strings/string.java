8eee4e7abb7bc02c74c05625adb61f95183409a1
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-01 13:57:44 +0530

    ..

--- src/strings/string.java
+++ src/strings/string.java
@@ -2,18 +2,23 @@ package strings;
 import  java.util.*;
 public class string {
     public static void main(String[] args){
-        StringBuilder sb= new StringBuilder("Tony");
-        System.out.println(sb);
-        // Character at index 0
-        System.out.println(sb.charAt(0));
-        // changing character at index 0
-        sb.setCharAt(0,'p');
-        System.out.println(sb);
-        // inserting
-        sb.insert(0,"s");
-        System.out.println(sb);
-        sb.insert(2,'n');
-        sb.append("e");
-        System.out.println(sb);
+        StringBuilder sb= new StringBuilder("Hello");
+//        System.out.println(sb);
+//        // Character at index 0
+//        System.out.println(sb.charAt(0));
+//        // changing character at index 0
+//        sb.setCharAt(0,'p');
+//        System.out.println(sb);
+//        // inserting
+//        sb.insert(0,"s");
+//        System.out.println(sb);
+//        sb.insert(2,'n');
+//        sb.append("e");
+//        System.out.println(sb);
+        // Reverse a string
+        for(int i=0;i<sb.length()/2;i++){
+            int front=i;
+            int back=sb.length()-1-i;
+        }
     }
 }
