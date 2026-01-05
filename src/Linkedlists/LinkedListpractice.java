8882459b7688827e5f3c1e8fafcbe66c8f52222f
Sagar Chhikara <sagarchhikara13570@gmail.com>
2026-01-05 23:27:20 +0530

    ..

--- src/Linkedlists/LinkedListpractice.java
+++ src/Linkedlists/LinkedListpractice.java
@@ -52,19 +52,19 @@ public class LinkedListpractice {
 //
 //
 //}
-    public static Node findNLast(Node head, int n) {
-        if (head == null) return null ;
-        Node temp = head;
-        int count = 0;
-        while (temp.next != null) {
-            temp = temp.next;
-            count++;
-        }
-        int x= count - n;
-        for (int i = 0; i < x; i++) {
-            temp = temp.next;
-        }
-        return temp;
-    }
+//    public static Node findNLast(Node head, int n) {
+//        if (head == null) return null ;
+//        Node temp = head;
+//        int count = 0;
+//        while (temp.next != null) {
+//            temp = temp.next;
+//            count++;
+//        }
+//        int x= count - n;
+//        for (int i = 0; i < x; i++) {
+//            temp = temp.next;
+//        }
+//        return temp;
+//    }
 
 }
