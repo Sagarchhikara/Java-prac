81963e9a9d70d8f5045ec0ffb9b1abf24c23aa3c
Sagar Chhikara <sagarchhikara13570@gmail.com>
2026-01-06 19:18:03 +0530

    //

--- src/Codechallenge/Code1.java
+++ src/Codechallenge/Code1.java
@@ -87,6 +87,23 @@ public class Code1 {
         }
         return unique;
     }
+    // next smallest element
+//    static int[] nextSmaller(int[] demand) {
+//        int n = demand.length;
+//        int[] result = new int[n];
+//        Stack<Integer> stack = new Stack<>();
+//
+//        for (int i = n - 1; i >= 0; i--) {
+//
+//            while (!stack.isEmpty() && stack.peek() >= demand[i]) {
+//                stack.pop();
+//            }
+//
+//            result[i] = stack.isEmpty() ? -1 : stack.peek();
+//            stack.push(demand[i]);
+//        }
+//        return result;
+//    }
 
 
 }
