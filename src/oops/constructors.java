23936701824536080a83bec0f29baeba2cf059f4
Sagar Chhikara <sagarchhikara13570@gmail.com>
2026-01-11 21:41:50 +0530

    ../

--- src/oops/constructors.java
+++ src/oops/constructors.java
@@ -2,45 +2,45 @@ package oops;
 import java.util.*;
 public class constructors {
     //Data members(fields/attributes)
-//    String name;
-//    int rollno;
-//    float marks;
-//    void displayINfo(){
-//        System.out.println("Name"+name);
-//        System.out.println("Rollno"+rollno);
-//        System.out.println("Marks"+marks);
-//       // OBjects
-//
-//    }
-//    public static void main(String[] args) {
-//        // Creating objects
-//        Student s1 = new Student();
-//        Student s2 = new Student();
-//
-//        // Setting values
-//        s1.name = "Alice";
-//        s1.rollNumber = 101;
-//        s1.marks = 85.5f;
-//
-//        s2.name = "Bob";
-//        s2.rollNumber = 102;
-//        s2.marks = 90.0f;
-//
-//        // Calling methods
-//        s1.displayInfo();
-//        s2.displayInfo();
-//    }
-//}
-//class Student {
-//    // Data members
-//    String name;
-//    int rollNumber;
-//    float marks;
-//
-//    // Method
-//    void displayInfo() {
-//        System.out.println("Name: " + name);
-//        System.out.println("Roll Number: " + rollNumber);
-//        System.out.println("Marks: " + marks);
-//    }
-//}
+    String name;
+    int rollno;
+    float marks;
+    void displayINfo(){
+        System.out.println("Name"+name);
+        System.out.println("Rollno"+rollno);
+        System.out.println("Marks"+marks);
+       // OBjects
+
+    }
+    public static void main(String[] args) {
+        // Creating objects
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        // Setting values
+        s1.name = "Alice";
+        s1.rollNumber = 101;
+        s1.marks = 85.5f;
+
+        s2.name = "Bob";
+        s2.rollNumber = 102;
+        s2.marks = 90.0f;
+
+        // Calling methods
+        s1.displayInfo();
+        s2.displayInfo();
+    }
+}
+class Student {
+    // Data members
+    String name;
+    int rollNumber;
+    float marks;
+
+    // Method
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+    }
+}
