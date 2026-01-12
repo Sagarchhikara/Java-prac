b76be31d1b70474422be3f05f57236861ea3c5f5
Sagar Chhikara <sagarchhikara13570@gmail.com>
2026-01-12 21:32:12 +0530

    //

--- src/Practiceproblems/practice3.java
+++ src/Practiceproblems/practice3.java
@@ -2,22 +2,22 @@ package Practiceproblems;
 import java.util.*;
 public class practice3 {
 
-
-    public class Main {
-        static void reverseInGroups(int[] heights, int n, int k) {
-            for (int i = 0; i < n; i += k) {
-                int left = i;
-                int right = Math.min(i + k - 1, n - 1);
-
-                while (left < right) {
-                    int temp = heights[left];
-                    heights[left] = heights[right];
-                    heights[right] = temp;
-                    left++;
-                    right--;
-                }
-            }
-        }
+//
+//    public class Main {
+//        static void reverseInGroups(int[] heights, int n, int k) {
+//            for (int i = 0; i < n; i += k) {
+//                int left = i;
+//                int right = Math.min(i + k - 1, n - 1);
+//
+//                while (left < right) {
+//                    int temp = heights[left];
+//                    heights[left] = heights[right];
+//                    heights[right] = temp;
+//                    left++;
+//                    right--;
+//                }
+//            }
+//        }
 
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
