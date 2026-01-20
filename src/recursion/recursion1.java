50c874cb4cf884c47a50f1e71e7342f7d74a7aad
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-20 21:41:01 +0530

    //

--- src/recursion/recursion1.java
+++ src/recursion/recursion1.java
@@ -1,8 +1,18 @@
 package recursion;
+import java.util.*;
 
 public class recursion1 {
     public static void main(String[] args) {
+        message();
 
+    }
+
+    private static void message() {
+        System.out.println("Hello World");
+        message1();
+    }
+    static  void message1() {
+        System.out.println("Hello World from message1");
     }
 
 }
