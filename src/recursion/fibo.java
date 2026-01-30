aca1c9596ec205eda12c7a8de040b3d829d4f1fc
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-01-30 09:45:01 +0530

    ..

--- src/recursion/fibo.java
+++ src/recursion/fibo.java
@@ -1,41 +1,47 @@
 package recursion;
 import java.util.*;
-//public class fibo {
-//    public static  void main(String[] args){
-//        System.out.println(fibo(4));
-//    }
-//    static int fibo(int n){
-//       if(n<2){
-//          return n;
-//      }
-//       return fibo(n-1)+fibo(n-2);// Doing recursively
-//
-//
-//    }
-//}
-// Doing Fibonacci series iteratively
 public class fibo {
-    public static void main(String[] args) {
+    public static  void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of terms: ");
+        System.out.print("Enter a number to find Fibonacci: ");
         int n = sc.nextInt();
-        if (n <= 0) {
-            System.out.println("Please enter a positive integer.");
-            sc.close();
-            return;
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibo(i)+"->");
         }
-
-        long a = 0, b = 1;
-        System.out.print("Fibonacci series: ");
-        for (int i = 1; i <= n; i++) {
-            System.out.print(a);
-            if (i < n) System.out.print(", ");
-            long next = a + b;
-            a = b;
-            b = next;
-        }
-        System.out.println();
         sc.close();
     }
+    static int fibo(int n){
+       if(n<2){
+          return n;
+      }
+       return fibo(n-1)+fibo(n-2);// Doing recursively
+
+
+    }
 }
+// Doing Fibonacci series iteratively
+//public class fibo {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter number of terms: ");
+//        int n = sc.nextInt();
+//        if (n <= 0) {
+//            System.out.println("Please enter a positive integer.");
+//            sc.close();
+//            return;
+//        }
+//
+//        long a = 0, b = 1;
+//        System.out.print("Fibonacci series: ");
+//        for (int i = 1; i <= n; i++) {
+//            System.out.print(a);
+//            if (i < n) System.out.print(", ");
+//            long next = a + b;
+//            a = b;
+//            b = next;
+//        }
+//        System.out.println();
+//        sc.close();
+//    }
+//}
 
