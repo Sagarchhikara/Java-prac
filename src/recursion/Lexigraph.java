987cf992057e10d7e78fbc8dc2f3f5fceed56021
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-02-18 09:16:50 +0530

    ,,

--- src/recursion/Lexigraph.java
+++ src/recursion/Lexigraph.java
@@ -1,27 +1,20 @@
-//package recursion;
-//import java.util.*;
-//import java.awt.*;
-//
-//public class Lexigraph {
-//    // function to generate lexicographical numbers upto n
-//    public static List<Integer> lexicalOrder(int n) {
-//        List<Integer> result = new ArrayList<>();
-//        for (int i = 1; i <= 9; i++) {
-//            dfs(i, n, result);
-//        }
-//        return result;
-//    }
-//    public static void  df(int current, int n, List<Integer> result) {
-//        if (current > n) {
-//            return;
-//        }
-//        result.add(current);
-//        for (int i = 0; i <= 9; i++) {
-//            int next = current * 10 + i;
-//            if (next > n) {
-//                break;
-//            }
-//            dfs(next, n, result);
-//        }
-//    }
-//}
+package recursion;
+
+public class Lexigraph {
+    public static void main(String[] args) {
+        String s = "1234";
+        printLexicographical(s, "");
+    }
+
+    public static void printLexicographical(String s, String prefix) {
+        if (s.length() == 0) {
+            System.out.println(prefix);
+            return;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            String remaining = s.substring(0, i) + s.substring(i + 1);
+            printLexicographical(remaining, prefix + ch);
+        }
+    }
+}
