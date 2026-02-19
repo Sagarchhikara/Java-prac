7f48b447ece21a4288799f7a69db92a54f53387b
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-02-19 10:17:44 +0530

    //

--- src/collectionframeworks/Maps.java
+++ src/collectionframeworks/Maps.java
@@ -9,18 +9,31 @@ public class Maps {
 //        students.put(3,"Daniel");
 //        System.out.println(students);
         // Find the frequency of characters in a string
-        String str="hello world";
-        HashMap<Character,Integer> freq=new HashMap<>();
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(freq.containsKey(ch)) {
-                freq.put(ch, freq.get(ch) + 1);
+//        String str="hello world";
+//        HashMap<Character,Integer> freq=new HashMap<>();
+//        for(int i=0;i<str.length();i++){
+//            char ch=str.charAt(i);
+//            if(freq.containsKey(ch)) {
+//                freq.put(ch, freq.get(ch) + 1);
+//            }
+//            else  {
+//                freq.put(ch, 1);
+//            }
+//
+//    }
+//        System.out.println(freq);
+//    }
+        // Find weather two numbers sum is equal to a target number
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 5;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+            if (map.containsKey(complement)) {
+                System.out.println("Pair found: " + arr[i] + " and " + complement);
             }
-            else  {
-                freq.put(ch, 1);
-            }
-    }
-        System.out.println(freq);
+            map.put(arr[i], i);
+        }
     }
 
 }
