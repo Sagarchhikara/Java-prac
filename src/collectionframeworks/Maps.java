d7ce42cff873167231a67646d235a8495812476c
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-02-19 09:50:58 +0530

    //

--- src/collectionframeworks/Maps.java
+++ src/collectionframeworks/Maps.java
@@ -8,5 +8,19 @@ public class Maps {
 //        students.put(2,"Ankit");
 //        students.put(3,"Daniel");
 //        System.out.println(students);
+        // Find the frequency of characters in a string
+        String str="hello world";
+        HashMap<Character,Integer> freq=new HashMap<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(freq.containsKey(ch)) {
+                freq.put(ch, freq.get(ch) + 1);
+            }
+            else  {
+                freq.put(ch, 1);
+            }
     }
+        System.out.println(freq);
+    }
+
 }
