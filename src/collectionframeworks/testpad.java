a7ba18892972a7858666ba0ff531edbe371ad610
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-03-11 10:08:39 +0530

    ///

--- src/collectionframeworks/testpad.java
+++ src/collectionframeworks/testpad.java
@@ -13,4 +13,17 @@ public class testpad {
 
 
     }
+    static void mirrorQueue(Queue<String> q)
+    {
+        Stack<String> s= new Stack<>();
+        int n=q.size();
+        for(int i=0;i<n;i++){
+            String x=q.remove();
+            s.push(x);
+            q.add(x);
+        }
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+    }
 }
