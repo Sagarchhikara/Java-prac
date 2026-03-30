00652ac6e4c97142c83e586bbb8f3b2d006cab29
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-03-30 21:43:13 +0530

    //

---
+++ src/stackqueue/inbuilteg.java
@@ -0,0 +1,16 @@
+package stackqueue;
+
+import  java.util.*;
+public class inbuilteg {
+    public static void main(String[] args){
+        Stack<Integer> stack =new Stack<>();
+        stack.push(1);
+        stack.push(3);
+        stack.push(10);
+        stack.push(12);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+
+        Queue<Integer> queue= new
+    }
+}
