0a3153ad008445329d333e2043d4462806b54cbc
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-04-06 09:46:43 +0530

    ///

---
+++ src/BitManupulation/bitAndORXor.java
@@ -0,0 +1,8 @@
+package BitManupulation;
+import java.util.*;
+public class bitAndORXor {
+    public static void main(String[] args){
+        System.out.println(51&42);
+        System.out.println(51|42);
+    }
+}
