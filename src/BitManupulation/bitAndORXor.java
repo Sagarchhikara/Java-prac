63f434cc9c6f3289a2db0c500f377b045ade5064
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-04-07 21:31:11 +0530

    //

--- src/BitManupulation/bitAndORXor.java
+++ src/BitManupulation/bitAndORXor.java
@@ -4,5 +4,16 @@ public class bitAndORXor {
     public static void main(String[] args){
         System.out.println(51&42);
         System.out.println(51|42);
+        System.out.println(countsetbits(5));
+
+    }
+    //  Write code to count the number of set bits in an integer n.
+    public static int countsetbits(int n){
+        int count=0;
+        while(n!=0){
+            n&=n-1;
+            count++;
+        }
+        return  count;
     }
 }
