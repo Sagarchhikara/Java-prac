1a22360ace476896bc17dec191f38bff0326ce73
Sagar Chhikara <sagarchhikara13570@gmail.com>
2026-04-13 09:48:22 +0530

    Create OOPS.java

---
+++ src/oops/OOPS.java
@@ -0,0 +1,26 @@
+package oops;
+
+public class OOPS {
+//    class Car {
+//        String color; // Instance variable
+//
+//        void drive() { // Method
+//            System.out.println("Driving");
+//        }
+//    }
+//
+//    public class Main {
+//        public static void main(String[] args) {
+//            Car myCar = new Car(); // 'myCar' is the object reference
+//            myCar.color = "Red";
+//        }
+//    }
+//    class Car {
+//        String color;
+//
+//        // Constructor
+//        Car(String initialColor) {
+//            color = initialColor;
+//        }
+//    }
+}
