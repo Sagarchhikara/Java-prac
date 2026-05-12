1976cd26737a1beabaf4ecb88b0344a53f4d1b1f
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-05-12 22:16:33 +0530

    ///

---
+++ src/Tree/BST.java
@@ -0,0 +1,30 @@
+package Tree;
+
+public class BST {
+    public class Node {
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    }
+    private Node root;
+    public BST() {
+
+    }
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+    public boolean isEmpty(){
+        return root == null;
+    }
+}
