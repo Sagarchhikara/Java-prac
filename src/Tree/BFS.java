5286d90018519eafdae665403da043c4120615a2
Sagarchhikara <sagarchhikara13570@gmail.com>
2026-05-21 09:36:09 +0530

    ??

--- src/Tree/BFS.java
+++ src/Tree/BFS.java
@@ -56,6 +56,48 @@ public class BFS {
         }
         return queue.peek();
     }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean reverse = false;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i=0; i < levelSize; i++) {
+                if (!reverse) { // added in the back if not revser
+                    TreeNode currentNode = queue.poll();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) {
+                        queue.addLast(currentNode.left);
+                    }
+                    if (currentNode.right != null) {
+                        queue.addLast(currentNode.right);
+                    }
+                } else {
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+
+                    if (currentNode.right != null) {
+                        queue.addFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null) {
+                        queue.addFirst(currentNode.left);
+                    }
+                }
+            }
+            reverse = !reverse;
+
+
+            result.add(currentLevel);
+        }
+        return result;
+    }
     }
 
 
